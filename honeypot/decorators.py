744960913ef2b82d3e524429bb513b0ed2fc41cc
Chris King <chris.king@netapp.com>
2013-04-08 09:49:57 -0400

    various quote forms now installed.

--- honeypot/decorators.py
+++ honeypot/decorators.py
@@ -73,3 +73,101 @@ def check_honeypot(func=None, field_name=None):
             return decorated(func)
         return decorator
     return decorated(func)
+
+
+def verify_honeypot_value_SellForm(request, field_name):
+    """
+        Verify that request.POST[field_name] is a valid honeypot.
+
+        Ensures that the field exists and passes verification according to
+        HONEYPOT_VERIFIER.
+    """
+    verifier = getattr(settings, 'HONEYPOT_VERIFIER', honeypot_equals)
+    dictToReturn = {}
+    if request.method == 'POST':
+        field = field_name or settings.HONEYPOT_FIELD_NAME
+        if field not in request.POST or not verifier(request.POST[field]):
+            resp = render_to_string('honeypot/honeypot_error.html',
+                                    {'fieldname': field})
+            form = SellForm(request.POST)
+            if form.is_valid():
+                dictToReturn['form'] = form
+                msg = EmailMultiAlternatives("Support Request - FROM PLCPART", form.getEmailBody(), fromEmail, [to_spamEmail])
+                msg.attach_alternative(render_to_response('surplusEmail.html', dictToReturn), 'text/html')
+                msg.send()
+            return HttpResponseBadRequest(resp)
+
+def check_honeypot_SellForm(func=None, field_name=None):
+    """
+        Check request.POST for valid honeypot field.
+
+        Takes an optional field_name that defaults to HONEYPOT_FIELD_NAME if
+        not specified.
+    """
+    # hack to reverse arguments if called with str param
+    if isinstance(func, basestring):
+        func, field_name = field_name, func
+
+    def decorated(func):
+        def inner(request, *args, **kwargs):
+            response = verify_honeypot_value_SellForm(request, field_name)
+            if response:
+                return response
+            else:
+                return func(request, *args, **kwargs)
+        return wraps(func)(inner)
+
+    if func is None:
+        def decorator(func):
+            return decorated(func)
+        return decorator
+    return decorated(func)
+
+def verify_honeypot_value_QuoteForm(request, field_name):
+    """
+        Verify that request.POST[field_name] is a valid honeypot.
+
+        Ensures that the field exists and passes verification according to
+        HONEYPOT_VERIFIER.
+    """
+    verifier = getattr(settings, 'HONEYPOT_VERIFIER', honeypot_equals)
+    dictToReturn = {}
+    if request.method == 'POST':
+        field = field_name or settings.HONEYPOT_FIELD_NAME
+        if field not in request.POST or not verifier(request.POST[field]):
+            resp = render_to_string('honeypot/honeypot_error.html',
+                                    {'fieldname': field})
+            form = QuoteForm(request.POST)
+            if form.is_valid():
+                dictToReturn['form'] = form
+                msg = EmailMultiAlternatives("Support Request - FROM PLCPART", form.getEmailBody(), fromEmail, [to_spamEmail])
+                msg.attach_alternative(render_to_response('surplusEmail.html', dictToReturn), 'text/html')
+                msg.send()
+            return HttpResponseBadRequest(resp)
+
+def check_honeypot_QuoteForm(func=None, field_name=None):
+    """
+        Check request.POST for valid honeypot field.
+
+        Takes an optional field_name that defaults to HONEYPOT_FIELD_NAME if
+        not specified.
+    """
+    # hack to reverse arguments if called with str param
+    if isinstance(func, basestring):
+        func, field_name = field_name, func
+
+    def decorated(func):
+        def inner(request, *args, **kwargs):
+            response = verify_honeypot_value_QuoteForm(request, field_name)
+            if response:
+                return response
+            else:
+                return func(request, *args, **kwargs)
+        return wraps(func)(inner)
+
+    if func is None:
+        def decorator(func):
+            return decorated(func)
+        return decorator
+    return decorated(func)
+
